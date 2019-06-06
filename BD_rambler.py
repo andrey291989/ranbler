41cbd87e9dbb2f75b9a6ee541c2474d68645c3fb
unknown <booribr@icloud.com>
2019-06-06 10:49:41 +0300

    second commit BR

--- BD_rambler.py
+++ BD_rambler.py
@@ -1,20 +1,26 @@
 import sqlite3
 import ram
+import sched, time
 
-# Подключение к БД.
-conn = sqlite3.connect('rambler.db')
-cursor = conn.cursor()
-
-# cписок групп в которых нужно узнать сколько подписчиком в ней
+# cписок групп в которых нужно узнать сколько подписчиком в ней.
 id = ['rambler', 'ramblermail', 'horoscopesrambler', 'championat',
       'championat.auto', 'championat_cybersport', 'livejournal', 'afisha']
-# Токен для подключения к API VK
+
+# Токен для подключения к API VK (безательный аргумент)
 toc = ''
+
+# Время с какой переодичностью нужно собирать данные (в секундах).
+t = 86400
 
 data = ram.namder(id, toc)
 
 
 def bd_rambler(data):
+    # Подключение к БД.
+    conn = sqlite3.connect('rambler.db')
+    cursor = conn.cursor()
+    print('')
+    print('Новый день, новые данные!')
     for tabl in range(len(data)):
         g = []
         name = data[tabl][0].replace('.', '_')
@@ -22,21 +28,31 @@ def bd_rambler(data):
         count = data[tabl][1]
         g.append(count)
         try:
-            # Создание таблицы
+            # Создание таблицы.
             cursor.execute(
                 """CREATE TABLE table_ram (id INTEGER PRIMARY KEY AUTOINCREMENT, name_grup TEXT, count INT, date REAL NOT NULL) """)
-            cursor.execute("""INSERT INTO table_ram (name_grup, count, date) VALUES ( ?, ?,CURRENT_DATE)""", g)
         except:
-            cursor.execute("""INSERT INTO table_ram (name_grup, count, date) VALUES ( ?, ?,CURRENT_DATE)""", g)
+            pass
+        cursor.execute("""INSERT INTO table_ram (name_grup, count, date) VALUES ( ?, ?, CURRENT_TIMESTAMP)""", g)
 
     cursor.execute("""SELECT * FROM table_ram """)
     gf = cursor.fetchall()
     for res in range(len(gf)):
         print(gf[res])
 
+    # Коммит данных и перкращение работы с БД.
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-bd_rambler(data)
 
-conn.commit()
-cursor.close()
-conn.close()
+# loop событий
+s = sched.scheduler(time.time, time.sleep)
+
+
+def some_times(t, data=data):
+    bd_rambler(data)
+    s.enter(t, 1, some_times, (t,))
+    s.run()
+
+some_times(t, data)
